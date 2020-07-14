8619101620cd06979ecfaa2f10c9bda5f18ecd8e
Bruno Oliveira Galvão de Arruda <bruno.ogarruda@gmail.com>
2020-07-14 09:21:43 -0300

    Initial commit

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,57 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+
+    if (
+      type === 'outcome' &&
+      value > (await transactionsRepository.getBalance()).total
+    ) {
+      throw new AppError(
+        'Should not be able to create outcome transaction without a valid balance',
+      );
+    }
+
+    let checkCategory = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategory) {
+      checkCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      await categoriesRepository.save(checkCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: checkCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
